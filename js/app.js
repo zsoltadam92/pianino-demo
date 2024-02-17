0af1fb5344956137599c1842f88add730cd433cf
zsoltadam92 <zsoltadam92@gmail.com>
2024-02-17 20:13:21 +0100

    style and js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,73 @@
-console.log(1234);
+const keys = [
+  { note: 'a', color: 'white' },
+  { note: 'w', color: 'black' },
+  { note: 's', color: 'white' },
+  { note: 'e', color: 'black' },
+  { note: 'd', color: 'white' },
+  { note: 'f', color: 'white' },
+  { note: 't', color: 'black' },
+  { note: 'g', color: 'white' },
+  { note: 'z', color: 'black' },
+  { note: 'h', color: 'white' },
+  { note: 'u', color: 'black' },
+  { note: 'j', color: 'white' },
+  { note: 'k', color: 'white' },
+  { note: 'o', color: 'black' },
+  { note: 'l', color: 'white' },
+  { note: 'p', color: 'black' },
+  { note: 'é', color: 'white' }
+];
+
+const pianoKeys = document.querySelector("#pianoKeys")
+const volumeSlider = document.querySelector("#customRange1")
+const switchKeysChars = document.querySelector("#flexSwitchCheckChecked")
+let currentVolume = 0.5;
+
+const renderPiano = () => {
+  const ul = document.createElement("ul")
+  ul.className = "d-flex list-unstyled mt-3"
+
+  keys.forEach(key => {
+    const li = document.createElement("li")
+    li.className = `key ${key.color}`
+    li.setAttribute("data-key", key.note)
+    li.innerHTML = `<span>${key.note}</span>`
+    ul.appendChild(li) 
+  })
+
+  pianoKeys.appendChild(ul)
+}
+
+
+const playAudio = (event) => {
+  const keyElement = document.querySelector(`.key[data-key="${event.key}"]`);
+  
+  if (keyElement) {
+    const audio = new Audio(`sounds/${event.key}.ogg`);
+    audio.volume = currentVolume
+    audio.play();
+    keyElement.classList.add("active"); 
+
+    setTimeout(() => {
+      keyElement.classList.remove("active");
+    }, 250);
+  }
+}
+
+document.addEventListener('keydown', playAudio);
+
+
+const handleVolume = (event) => {
+  currentVolume = event.target.value
+}
+
+volumeSlider.addEventListener("input", handleVolume)
+
+
+const handleSwitch = () => {
+  document.querySelectorAll(".key").forEach((key) => key.classList.toggle("hide"));
+}
+
+switchKeysChars.addEventListener("click", handleSwitch)
+
+renderPiano()
